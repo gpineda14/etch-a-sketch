a6b148cdd0e4b6892e7cec69dcb364810f46564c
Guillermo Pineda <pinedag395@gmail.com>
2017-07-11 14:44:50 -0700

    Added functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,43 @@
 var size = 16;
-for (i = 0; i < size; i++) {
-  var row = document.createElement("tr");
-  row.style.class = "row";
-  row.innerHTML = "row";
-  document.getElementById("pad").appendChild(row);
-  for (j = 0; j < size; j++) {
-    var column = document.createElement("td");
-    column.innerHTML = "column";
-    document.getElemenyById("row").appendChild(column);
+$(document).ready(function() {
+  createGrid(size);
+  $("td.entry").hover(function() {
+    $(this).addClass("hovered");
+  });
+});
+
+function createGrid(size) {
+  var currSize = size;
+  for (i = 0; i < currSize; i++) {
+    var row = "<tr class='row'></tr>";
+    $(row).appendTo(".pad");
   }
+  for (j = 0; j < currSize; j++) {
+    var entry = "<td class='entry'></td>";
+    $(entry).appendTo('.row');
+  }
+  sizeEntries();
+}
+
+function newGrid() {
+  $("td.entry").removeClass("hovered");
+  $(".pad").empty();
+  setGridSize();
+  createGrid(size);
+  $("td.entry").hover(function() {
+    $(this).addClass("hovered");
+  });
+}
+
+function setGridSize() {
+  size = prompt("Enter size of grid:", "16");
+  if (size == null || size == "") {
+    size = 16;
+  }
+}
+
+function sizeEntries(size) {
+  var height = 720 / size;
+  var width = 720 / size;
+  $("td.entry").css({"height": height, "width": width});
 }
